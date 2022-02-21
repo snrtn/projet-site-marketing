f497364dca21420818d7e1e0f88147ac21555c9b
snrtn <snrtn94@gmail.com>
2022-02-21 10:56:02 +0100

    add navbar

---
+++ client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// style
+import { Container } from "./navbar.style";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return <Container>Navbar</Container>;
+};
+
+export default Navbar;
