907efaffe3bc090fc160490722315a9c6a23731a
snrtn <snrtn94@gmail.com>
2022-02-21 16:58:44 +0100

    change code

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 // style
-import { Container } from "./navbar.style";
+import {
+  Container,
+  Wrapper,
+  Left,
+  LogoContainer,
+  Image,
+  TitleContainer,
+  Title,
+  SubTitle,
+  Right,
+  Menu,
+  Item,
+  Icon,
+  Burger,
+} from "./navbar.style";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
-  return <Container>Navbar</Container>;
+  const [open, setOpen] = useState(false);
+  const toggleOpen = () => setOpen((value) => !value);
+
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <LogoContainer>
+            <Image alt="GileRibeiro" src="img/GileRibeiro.jpg" />
+            <TitleContainer>
+              <Title>쌍문주짓수</Title>
+              <SubTitle>질리히베이로 코리아</SubTitle>
+            </TitleContainer>
+          </LogoContainer>
+        </Left>
+        <Right>
+          <Menu open={open}>
+            <Item onClick={toggleOpen}>홈</Item>
+            <Item onClick={toggleOpen}>소개</Item>
+            <Item onClick={toggleOpen}>주간일정</Item>
+            <Item onClick={toggleOpen}>이용시설</Item>
+            <Item onClick={toggleOpen}>오시는길</Item>
+            <Item onClick={toggleOpen}>상담</Item>
+          </Menu>
+
+          <Icon onClick={() => setOpen(!open)}>
+            <Burger open={open}></Burger>
+            <Burger open={open}></Burger>
+            <Burger open={open}></Burger>
+          </Icon>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
--- client/src/components/navbar/navbar.style.ts
+++ client/src/components/navbar/navbar.style.ts
@@ -1,3 +1,164 @@
 import styled from "styled-components";
+import variable from "../../styles/variable";
+import { r768px, r1280px } from "../../styles/responsive";
 
-export const Container = styled.div``;
+interface open {
+  readonly open: boolean;
+}
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: center;
+`;
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 1350px;
+  height: 65px;
+`;
+export const Left = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  padding-left: 20px;
+  ${r1280px({
+    flex: "0.2",
+  })}
+`;
+export const LogoContainer = styled.article`
+  display: flex;
+  align-items: center;
+  width: 180px;
+  height: 100%;
+  cursor: pointer;
+`;
+export const Image = styled.img`
+  width: 45px;
+  height: 45px;
+`;
+export const TitleContainer = styled.div`
+  margin-left: 20px;
+`;
+export const Title = styled.h1`
+  font-size: ${variable.d18px};
+  ${r768px({
+    fontSize: variable.m24px,
+  })}
+`;
+export const SubTitle = styled.h2`
+  font-size: ${variable.d12px};
+  ${r768px({
+    fontSize: variable.m16px,
+  })}
+`;
+
+export const Right = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  position: relative;
+  ${r768px({
+    flex: "0.2",
+    paddingRight: "20px",
+  })}
+`;
+
+export const Menu = styled.div<open>`
+  font-family: "Zilla Slab", sans-serif;
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  position: absolute;
+  top: 0;
+  justify-content: flex-end;
+  align-items: center;
+  margin-left: 20px;
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    position: fixed;
+    right: ${(props) => (props.open ? "0px" : "-250px")};
+    top: 0px;
+    height: 100vh;
+    width: 250px;
+    padding-top: 3.5rem;
+    margin-left: 0px;
+    justify-content: normal;
+    transition: all 0.3s linear;
+    border-left: 1px solid ${variable.lightgray};
+  }
+`;
+export const Item = styled.a`
+  display: inline-block;
+  margin: 0px 20px;
+  cursor: pointer;
+  position: relative;
+  font-size: ${variable.d16px};
+  ${r768px({
+    fontSize: variable.m24px,
+    marginTop: "20px",
+  })}
+  &:before {
+    @media (hover: hover) {
+      content: "";
+      position: absolute;
+      background-color: ${variable.orange};
+      height: 1px;
+      width: 0;
+      bottom: -3px;
+      transition: all 0.3s linear;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
+  &:hover:before {
+    @media (hover: hover) {
+      width: 100%;
+    }
+  }
+`;
+
+export const Icon = styled.div`
+  width: 30px;
+  height: 20px;
+  z-index: 99;
+  display: none;
+  cursor: pointer;
+  position: relative;
+  font-family: "Zilla Slab", sans-serif;
+  position: fixed;
+  ${r768px({
+    display: "flex",
+  })}
+`;
+
+export const Burger = styled.div<open>`
+  width: 30px;
+  height: 3px;
+  background: ${variable.black};
+  border-radius: 10px;
+  transition: all 0.3s linear;
+  cursor: pointer;
+  position: absolute;
+
+  @media (prefers-color-scheme: dark) {
+    background: ${variable.white};
+  }
+
+  &:nth-child(1) {
+    top: ${(props) => (props.open ? "50%" : "0")};
+    transform: ${(props) => (props.open ? "rotate(45deg)" : "rotate(0)")};
+  }
+  &:nth-child(2) {
+    top: 50%;
+    transform: ${(props) =>
+      props.open ? "translateX(100%)" : "translateX(0)"};
+    opacity: ${(props) => (props.open ? 0 : 1)};
+  }
+  &:nth-child(3) {
+    top: ${(props) => (props.open ? "50%" : "100%")};
+    transform: ${(props) => (props.open ? "rotate(-45deg)" : "rotate(0)")};
+  }
+`;
