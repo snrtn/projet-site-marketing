1841a99f07c9f7974335b93ad7b3374ba7dde430
snrtn <snrtn94@gmail.com>
2022-02-21 10:55:54 +0100

    add footer

---
+++ client/src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+// style
+import { Container } from "./footer.style";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return <Container>Footer</Container>;
+};
+
+export default Footer;
